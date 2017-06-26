668592c01b4b86360b2b7ecb0f1da8947e6fbb6b
Nabeel Omer <nabeelkomer@gmail.com>
2017-06-26 12:02:44 +0530

    Move to rust part 2

--- target/debug/MailShell.d
+++
@@ -1,1 +0,0 @@
-/home/nabeel/src/MailShell/target/debug/MailShell: /home/nabeel/src/MailShell/atarashii_imap/src/error.rs /home/nabeel/src/MailShell/atarashii_imap/src/lib.rs /home/nabeel/src/MailShell/src/main.rs
